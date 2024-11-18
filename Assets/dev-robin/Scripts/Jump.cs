d5da3f53fbb1f9181ae36f7c526544ec8d3b31f9
Robi-1224 <230087@student.glu.nl>
2024-11-18 13:06:19 +0100

    base jump mechanic physics based

--- Assets/dev-robin/Scripts/Jump.cs
+++ Assets/dev-robin/Scripts/Jump.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Jump : MonoBehaviour
 {
+    [SerializeField] InputActionReference jump;
+    private bool isOnGround = true;
+    [SerializeField] float jumpForce;
     // Start is called before the first frame update
+
+    private void OnEnable()
+    {
+        jump.action.performed += Jumping;
+    }
+
+    private void OnDisable()
+    {
+        jump.action.performed -= Jumping;
+    }
     void Start()
     {
         
@@ -14,5 +28,25 @@ public class Jump : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void Jumping(InputAction.CallbackContext context)
+    {
+       
+        if (isOnGround)
+        {
+            isOnGround = false;
+            gameObject.GetComponent<Rigidbody>().AddForce(0,jumpForce,0,ForceMode.Impulse);
+        }
+
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isOnGround = true;
+        }
     }
 }
