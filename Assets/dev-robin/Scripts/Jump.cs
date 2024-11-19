c906dc1aa25cb662e779245590a5b7ef11afab6f
Robi-1224 <230087@student.glu.nl>
2024-11-19 13:23:49 +0100

    ground pound with hitboxes working

--- Assets/dev-robin/Scripts/Jump.cs
+++ Assets/dev-robin/Scripts/Jump.cs
@@ -2,29 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Jump : MonoBehaviour
 {
     //references
-    [SerializeField] InputActionReference jump;
+    [SerializeField] InputActionReference jump,groundPound;
+    private Rigidbody rb;
 
     // variables
+    [SerializeField] int groundPoundForce;
+    [SerializeField] float groundPoundRadius;
     private bool isOnGround = true;
+
     private int jumpAmount = 2;
+
     [SerializeField] float jumpForce;
+
+    [SerializeField] LayerMask hitLayer;
+
 
     private void OnEnable()
     {
         jump.action.performed += Jumping;
+        groundPound.action.performed += GroundPound;
     }
 
     private void OnDisable()
     {
         jump.action.performed -= Jumping;
+        groundPound.action.performed -= GroundPound;
     }
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -35,26 +46,55 @@ public class Jump : MonoBehaviour
 
     private void Jumping(InputAction.CallbackContext context)
     {
-        if (isOnGround)
+        if (jumpAmount > 0)
         {
             jumpAmount--;
-            gameObject.GetComponent<Rigidbody>().AddForce(0,jumpForce,0,ForceMode.Impulse);
-            gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        }
-
-        //this is right after the player double jumps
-        if(jumpAmount == 0)
-        {
             isOnGround = false;
-            jumpAmount = 2;
+            rb.AddForce(0,jumpForce,0,ForceMode.Impulse);
+            rb.velocity = Vector3.zero;
+        }
+        
+    }
+
+    private void GroundPound(InputAction.CallbackContext context)
+    {
+        if(!isOnGround)
+        {
+            //ground pound animations still needs to be added
+            StartCoroutine(GroundPoundEnum());
         }
     }
 
+    private IEnumerator GroundPoundEnum()
+    {
+        WaitForSeconds wait = new WaitForSeconds(.5f);
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+
+        yield return wait;
+        rb.useGravity = true;
+        rb.AddForce(0,-groundPoundForce,0,ForceMode.Impulse);
+
+        yield return new WaitForSeconds(.25f);
+        // play ground pound vfx
+        Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, groundPoundRadius,hitLayer);
+      
+        foreach (Collider collider in hitColliders)
+        {
+            collider.gameObject.SetActive(false);
+        }
+        
+    }
+    private void OnDrawGizmos()
+    {
+       // the ground pound sphere for debugging Gizmos.DrawWireSphere(gameObject.transform.position, groundPoundRadius);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
             isOnGround = true;
+            jumpAmount = 2;
         }
     }
 }
