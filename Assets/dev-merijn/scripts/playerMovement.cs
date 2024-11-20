0d6c3c415d4fd644b8309808b302660a12a6314a
merijn515 <pelsmerijn@gmail.com>
2024-11-20 09:25:52 +0100

    tijdelijke movement

--- Assets/dev-merijn/scripts/playerMovement.cs
+++ Assets/dev-merijn/scripts/playerMovement.cs
@@ -11,7 +11,8 @@ public class playerMovement : MonoBehaviour
     private float speed;
     [SerializeField]
     private Vector3 testMove;
-    [SerializeField] InputActionReference movement;
+
+/*    [SerializeField] InputActionReference movement;*/
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +24,11 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //transform.forward = playerRb.velocity;
     }
     public void Move(InputAction.CallbackContext context)
     {
-        testMove = context.action.ReadValue<Vector3>();
+        testMove = context.action.ReadValue<Vector3>().normalized;
         //Debug.Log(testMove);
         if (context.performed)
         {
@@ -36,6 +37,11 @@ public class playerMovement : MonoBehaviour
         else
         {
             playerRb.velocity = Vector3.zero;
+        }
+
+        if (playerRb.velocity != Vector3.zero)
+        {
+        transform.right = playerRb.velocity/* + new Vector3(0f, 0f, 90f)*/;
         }
         //Debug.Log(playerRb.velocity);
     }
