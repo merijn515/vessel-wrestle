a7a5e8bdc38e0377a33daaa1cc1ea11c92f855d1
Robi-1224 <145546915+Robi-1224@users.noreply.github.com>
2024-11-22 15:16:45 +0100

    ragdoll working added to prefab, animations also work

--- Assets/dev-robin/Scripts/HealthController.cs
+++ Assets/dev-robin/Scripts/HealthController.cs
@@ -7,8 +7,9 @@ public class HealthController : MonoBehaviour
     public int playerHealth;
     private Rigidbody rb;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        RagdollPartsDisabled();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -23,9 +24,43 @@ public class HealthController : MonoBehaviour
         if(playerHealth == 0)
         {
             //ragdoll still needs to be made
+            RagdollActive();
             Debug.Log("dead");
         }
 
       
+    }
+
+    private void RagdollPartsDisabled()
+    {
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+        Rigidbody[] rigids = GetComponentsInChildren<Rigidbody>();
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = false;
+        }
+
+        foreach(Rigidbody rigid in rigids)
+        {
+            rigid.isKinematic = true;
+        }
+    }
+
+    private void RagdollActive()
+    {
+        gameObject.GetComponent<Animator>().enabled = false;
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+        Rigidbody[] rigids = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = true;
+        }
+
+        foreach (Rigidbody rigid in rigids)
+        {
+            rigid.isKinematic = false;
+        }
+
     }
 }
