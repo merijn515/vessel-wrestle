7bb2a20a6b8cbe5df0a7665fef894fe1e88f9b73
Robi-1224 <230087@student.glu.nl>
2024-12-02 13:57:21 +0100

    item spawn

--- Assets/dev-robin/Scripts/ItemSpawn.cs
+++ Assets/dev-robin/Scripts/ItemSpawn.cs
@@ -6,10 +6,12 @@ public class ItemSpawn : MonoBehaviour
 {
     [SerializeField] List<GameObject> itemList;
     [SerializeField] List<Transform> spawnList;
+    [SerializeField] Transform middleTrans;    
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnItems());
+        Instantiate(itemList[Random.Range(0, itemList.Count)],middleTrans);
     }
 
     // Update is called once per frame
@@ -21,7 +23,10 @@ public class ItemSpawn : MonoBehaviour
     private IEnumerator SpawnItems()
     {
         WaitForSeconds wait = new WaitForSeconds(15);
-        yield return wait;
-
+        
+        while (true) {
+            yield return wait;
+            Instantiate(itemList[Random.Range(0, itemList.Count)], spawnList[Random.Range(0, spawnList.Count)].transform.position,Quaternion.identity);
+        }
     }
 }
